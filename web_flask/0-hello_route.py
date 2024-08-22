94bb4418ac12bf06267dc473b0aefc9e4706b69c
ikeali <ikechukwualisigwe01@gmail.com>
2024-08-22 01:31:03 +0100

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""A script that starts a flask web application
+"""
+start Flask application
 """
 
 from flask import Flask
-app = Flask("__name__")
-
-@app.route('/airbnb-onepage/')
-def hello():
-    """Return a given string"""
-    return 'Hello, this is the AirBnB one-page app!'
+app = Flask(__name__)
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+@app.route('/airbnb-onepage/', strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
